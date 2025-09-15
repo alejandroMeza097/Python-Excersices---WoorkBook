9755cdf5eba87d435cdc0da4c7753a5bfed3cd1b
Alex Meza <alex.meza.5471@gmail.com>
2025-09-15 11:49:21 -0600

    solucion al ejercicio 4

--- execercise_4.py
+++ execercise_4.py
@@ -5,3 +5,9 @@ Create a program that reads the length and width of a farmer’s field from the 
 feet. Display the area of the field in acres.
 Hint: There are 43,560 square feet in an acre.
 '''
+print("Problem Number : 4")
+width_in_feet : float = float(input("Enter the value of width in feet : "))
+length_in_feet : float = float(input("Enter the value of length in feet : "))
+rate_of_conversion = (1/43560)
+area_in_acres = width_in_feet * length_in_feet * rate_of_conversion
+print(f"The area of the field in acres is {area_in_acres:.3f}")
