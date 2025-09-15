48bc19dbe4c36094860c2052540ce3b466948216
Alex Meza <alex.meza.5471@gmail.com>
2025-09-15 11:24:02 -0600

    Creating excercise number 4 and adding gitignore

---
+++ execercise_4.py
@@ -0,0 +1,7 @@
+'''
+Exercise 4: Area of a Field
+(Solved—15 Lines)
+Create a program that reads the length and width of a farmer’s field from the user in
+feet. Display the area of the field in acres.
+Hint: There are 43,560 square feet in an acre.
+'''
