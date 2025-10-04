7ac4a1af1f00b4c7a1421fb2143d7c0a852ec7c3
Alex Meza <alex.meza.5471@gmail.com>
2025-10-03 23:38:32 -0600

    Text of exercise number 19

---
+++ exercise_19.py
@@ -0,0 +1,12 @@
+'''
+Exercise 19: Free Fall
+(Solved—16 Lines)
+Create a program that determines how quickly an object is traveling when it hits the
+ground. The user will enter the height from which the object is dropped in meters (m).
+Because the object is dropped its initial speed is 0 m/s. Assume that the acceleration
+due to gravity is 9.8 m/s2 . You can use the formula vf =
+√
+v2
+i + 2ad to compute the
+final speed, vf , when the initial speed, vi , acceleration, a, and distance, d, are known.
+'''
