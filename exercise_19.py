d4f8626507c7a57bdf14687d9428f419d8ed5ec9
Alex Meza <alex.meza.5471@gmail.com>
2025-10-05 19:38:22 -0600

    Solution to exercise number 19

--- exercise_19.py
+++ exercise_19.py
@@ -4,9 +4,14 @@ Exercise 19: Free Fall
 Create a program that determines how quickly an object is traveling when it hits the
 ground. The user will enter the height from which the object is dropped in meters (m).
 Because the object is dropped its initial speed is 0 m/s. Assume that the acceleration
-due to gravity is 9.8 m/s2 . You can use the formula vf =
-√
-v2
-i + 2ad to compute the
+due to gravity is 9.8 m/s2 . You can use the formula 
+vf = √v2i + 2ad to compute the
 final speed, vf , when the initial speed, vi , acceleration, a, and distance, d, are known.
 '''
+from math import sqrt,pow
+print("Solution to exercise number 19")
+height_in_meters : float = float(input("Enter the height in meters from which the object is dropped : "))
+ACELERATION_GRAVITY = 9.81 # m/s^2
+INICIAL_VELOCITY = 0 # m/s
+final_velocity = sqrt(pow(INICIAL_VELOCITY,2) + 2 * ACELERATION_GRAVITY * height_in_meters)
+print(f'The velocity of the object when hits the ground is : {final_velocity:.2f} [m/s]')
